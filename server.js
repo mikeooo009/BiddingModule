25ea2433170de1c8f332bb4a112c9bb852f00881
mikeooo009 <mike.java.code@gmail.com>
2024-11-23 10:08:09 +0100

    third

--- server.js
+++ server.js
@@ -149,11 +149,6 @@ server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
-const {
-  saveBidToDatabase,
-  getHighestBidForAuction,
-} = require('./services/dbService');
  
 
 const {
